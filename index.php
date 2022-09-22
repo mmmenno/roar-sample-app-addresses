04c09310b59ca6a13e8f2b5f233142fbc903223b
Menno den Engelse <menno@denengelse.nl>
2022-09-22 11:35:30 +0200

    plausible

--- index.php
+++ index.php
@@ -20,6 +20,9 @@ include("functions.php");
   <link rel="stylesheet" href="https://unpkg.com/leaflet@1.1.0/dist/leaflet.css" integrity="sha512-wcw6ts8Anuw10Mzh9Ytw4pylW8+NAD4ch3lqm9lzAsTxg0GFeJgoAtxuCLREZSC5lUXdVyo/7yfsqFjQ4S+aKw==" crossorigin=""/>
   <script src="https://unpkg.com/leaflet@1.1.0/dist/leaflet.js" integrity="sha512-mNqn2Wg7tSToJhvHcqfzLMU6J4mkOImSPTxVZAdo+lcPlk+GhZmYgACEe0x35K7YzW1zJ7XyJV/TT1MrdXvMcA==" crossorigin=""></script>
 
+
+  <script async defer data-domain="hicsuntleones.nl" src="https://plausible.io/js/plausible.js"></script>
+
   <link rel="stylesheet" href="styles.css" />
 
   <style type="text/css">
